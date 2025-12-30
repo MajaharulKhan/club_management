756ff1f55b0866ac2b8d37bf7648eb2662fb1d07
Majaharul <majaharul8145@gmail.com>
2025-12-30 20:02:03 +0600

    hello

--- club_management/club_management/doctype/event_participant/event_participant.js
+++ club_management/club_management/doctype/event_participant/event_participant.js
@@ -9,4 +9,5 @@ frappe.ui.form.on("Event Participant", {
             })
         }
 	},
+    
 });
--- club_management/club_management/doctype/event_participant/event_participant.py
+++ club_management/club_management/doctype/event_participant/event_participant.py
@@ -7,10 +7,18 @@ from frappe.model.document import Document
 
 class EventParticipant(Document):
 	def before_validate(self):
+
+
 		if not self.club_member:
 			member = frappe.db.get_list('Club Member', pluck='name', page_length=1)
+			frappe.msgprint(str(member))
 			if member:
 				self.club_member = member[0]
+		if frappe.db.exists("Event Participant",{"event": self.event, "club_member": self.club_member}):
+			frappe.throw("This member is already registered for this event.")
+		
+			
+		
 	
 	@frappe.whitelist()
 	def mark_as_paid(self):
